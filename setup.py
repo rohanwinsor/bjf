2b5f436f445a88167dcfd389562a811236d9d48d
rohanwinsor <rohan.w.charles@gmail.com>
2022-04-15 09:57:42 +0530

    updated version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 setup(
     name="bjf",
